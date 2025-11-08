0cbff7d07599c2a6dd1585a2fddfbdbfada03dcc
menigit <74985102+menigit@users.noreply.github.com>
2025-11-08 14:59:28 +0200

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'grocery-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './voucher.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
